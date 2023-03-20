a4a4817d0a040f7d0d7682fc40c30c7d0225c5c4
Chronarr <Lodberg@gmail.com>
2023-03-20 21:26:47 +0100

    Updated Search Header

--- src/components/HomeHeader.jsx
+++ src/components/HomeHeader.jsx
@@ -9,7 +9,7 @@ export default function HomeHeader() {
                 <Link className='hover:underline' href="/">Gmail</Link>
                 <Link className='hover:underline' href="/">Images</Link>
                 <CgMenuGridO className='text-4xl bg-transparent hover:bg-gray-300 rounded-full p-2' />
-                <button className='bg-blue-600 text-white px-6 py-2 font-medium rounded hover:brightness-105 hover:shadow-lg '>Sign in</button>
+                <button className='bg-blue-600 text-white text-sm px-6 py-2 font-medium rounded hover:brightness-105 hover:shadow-lg '>Sign in</button>
             </div>
         </header>
     )
--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import SearchBox from './SearchBox';
+import { RxGear } from "react-icons/rx";
+import { CgMenuGridO } from "react-icons/cg";
+import SearchHeaderOptions from './SearchHeaderOptions';
 
 export default function SearchHeader() {
     return (
-        <div>SearchHeader</div>
-    )
-}
+        <header className='sticky top-0 bg-white'>
+
+            <div className='flex w-full p-6 items-center justify-between'>
+
+                <Link href={"/"}>
+                    <Image src={"/googlelogo_color_160x56dp.png"} width="92" height="30"></Image>
+                </Link>
+
+                <div className='flex-1'>
+                    <SearchBox />
+                </div>
+                <div className='hidden sm:flex space-x-1'>
+                    <RxGear className='header-icon' />
+                    <CgMenuGridO className='header-icon' />
+                    <button className='bg-blue-600 text-white text-sm px-6 py-2 font-medium rounded hover:brightness-105 hover:shadow-lg transition-all'>Sign in</button>
+                </div>
+
+            </div>
+            <SearchHeaderOptions />
+        </header >
+    );
+};
