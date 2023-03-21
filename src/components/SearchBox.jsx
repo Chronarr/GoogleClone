7f72ee484abe0779d103be9b06c1a26857af0ca8
Chronarr <Lodberg@gmail.com>
2023-03-21 20:13:07 +0100

    Updated searchbox function

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+"use client"
+
+import React, { useState } from 'react'
+import { RxCross1 } from "react-icons/rx"
+import { BsFillMicFill } from "react-icons/bs";
+import { AiOutlineSearch } from "react-icons/ai";
+import { useSearchParams } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 
 export default function SearchBox() {
+
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const searchTerm = searchParams.get("search");
+    const [search, setSearch] = useState(searchTerm || "");
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (!search.trim()) return;
+        router.push(`/search/web?search=${search}`)
+    }
+
     return (
-        <div>SearchBox</div>
+        <form className='flex w-full border shadow-lg border-gray-200 rounded-full h-12 items-center px-4 ml-4 flex-grow max-w-3xl' onSubmit={handleSubmit}>
+            <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} onSubmit={handleSubmit} className='outline-none w-full' />
+            <RxCross1 className='text-xl cursor-pointer text-gray-400 mr-2' onClick={() => setSearch("")} alt="X" />
+            <BsFillMicFill className='cursor-pointer text-3xl pl-2 border-l-2 mr-2 border-gray-300' alt="Mic" />
+            <AiOutlineSearch className='cursor-pointer text-2xl' alt="Mag" onClick={handleSubmit} />
+        </form>
     )
 }
