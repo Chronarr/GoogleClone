617ace16c85391ed25dc4d2ffa08c90aa9d165b7
Chronarr <Lodberg@gmail.com>
2023-03-26 21:29:17 +0200

    Updated for vercel

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,11 +1,11 @@
+export const dynamic = "force-dynamic";
+
 import WebSearchResults from '@/components/WebSearchResults';
 import Link from 'next/link';
 import React from 'react'
 
 export default async function WebSearchPage({ searchParams }) {
-
     const startIndex = searchParams.start || 1;
-
     const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY_GOOGLE}&cx=${process.env.CX_KEY}&q=${searchParams.search}&start=${startIndex}`)
     if (!response.ok) {
         throw new error("Something went wrong!")
